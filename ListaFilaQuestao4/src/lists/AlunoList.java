69d01cc7e2723c67086c7f41df6dd37de3d479f8
pbelem <pedro.belem@ucsal.edu.br>
2025-05-19 19:08:00 -0300

    questao 5

--- ListaFilaQuestao4/src/lists/AlunoList.java
+++ ListaFilaQuestao4/src/lists/AlunoList.java
@@ -2,6 +2,7 @@ package lists;
 
 import entities.Aluno;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -11,18 +12,22 @@ Scanner input = new Scanner(System.in);
     Queue<Aluno> fila = new LinkedList<>();
 
     public void InserirAlunos(Aluno aluno1) {
+        //nn consegui fazer ordenando por idade mas vou tentar dnv dps, vi que tem uma forma
+        // nativa do java pra fazer isso mas vou tentar fazer por lógica mesmo que aprende melhor
         System.out.println("Informe o nome do aluno: ");
         String nome = input.nextLine();
         System.out.println("Informe a idade do aluno: ");
         int idade = input.nextInt();
         input.nextLine();
         Aluno aluno = new Aluno(nome, idade);
-        if(aluno.getIdade() < )
         fila.add(aluno);
     }
 
     public void ConsultarAlunos() {
-//nn consegui fazer ordenando por idade mas vou tentar dnv dps, vi que tem uma forma nativa do java pra fazer isso mas vou tentar fazer por lógica mesmo
+        Iterator<Aluno> it = fila.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
     }
 }
 
--- ListaFilaQuestao5/src/aplication/Program.java
+++ ListaFilaQuestao5/src/aplication/Program.java
@@ -1,4 +1,45 @@
 package aplication;
 
+import lists.PacienteLists;
+
+import java.util.Scanner;
+
 public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        PacienteLists pacientes = new PacienteLists();
+
+        while (true) {
+            System.out.println("O que deseja fazer?\n(1)Adicionar um novo paciente\n(2)Atender paciente\n(3)Verificar se há paciente\n(4)Próximo paciente a ser atendido" +
+                    "\n(5)Quantidade de pacientes que aguardam atendimento\n(6)Sair");
+
+            byte opcao = input.nextByte();
+            input.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    pacientes.inserirPaciente();
+                    break;
+                case 2:
+                    pacientes.atenderPaciente();
+                    break;
+                case 3:
+                    pacientes.temPaciente();
+                    break;
+                case 4:
+                    pacientes.proximoPacienteSerAtendido();
+                    break;
+                case 5:
+                    pacientes.numeroPacienteAguardandoAtendimento();
+                    break;
+                case 6:
+                    System.out.println("Encerrando o programa...");
+                    return;
+                default:
+                    System.out.println("Resposta fora do intervalo solicitado");
+            }
+        }
+    }
 }
